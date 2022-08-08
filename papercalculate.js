ce26ffbdad6f1cd3e884e2008293df5cf7f09c71
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-08-09 00:56:38 +0600

    papercalculate code are adedd

---
+++ papercalculate.js
@@ -0,0 +1,10 @@
+function paperRequierment(b1,b2,b3){
+    const a=b1*100;
+    const b=b2*200;
+    const c=b3*300;
+    const sum=a+b+c;
+    return sum;
+
+}
+var book1=1,book2=1,book3=1;
+console.log(paperRequierment(book1,book2,book3))
