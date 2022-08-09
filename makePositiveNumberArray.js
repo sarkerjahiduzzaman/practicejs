a976ffa0345bd878b2f1021d483350c0b1e97e63
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-08-09 09:53:00 +0600

    find positive number on an array

---
+++ makePositiveNumberArray.js
@@ -0,0 +1,18 @@
+function onlyPositive(number){
+    let newArray=[]
+    for(let i=0;i<number.length;i++)
+    {
+        if(number[i]>=0)
+        {
+            newArray.push(number[i])
+        }
+        else
+        {
+            break;
+        }
+    }
+    return newArray
+
+}
+var numbers=[15,58,22,99,88,55,22,-55,44,55,88]
+console.log(onlyPositive(numbers))
